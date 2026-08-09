3ec32dd1be2c1574ccd4af746c1afac367123e6c
obenmbar <othmane.benmbarek.20@edu.uiz.ac.ma>
2026-08-09 04:32:25 +0100

    ajoute les caracterstique de la vehicule

---
+++ smart_road/src/vehicle.rs
@@ -0,0 +1,58 @@
+#[derive(Debug,Clone,PartialEq)]
+pub enum Direction {
+    North,
+    South,
+    East,
+    West,
+}
+
+#[derive(Debug, Clone)]
+pub struct Vehicle {
+    pub id: u32,
+    pub distance: f32,
+    pub total_distance: f32 ,
+    pub time: f32,
+    pub velocity: f32,
+    pub lane: char,
+    pub direction:Direction,
+}
+
+impl Vehicle {
+    pub fn new(id: u32, distance: f32, time: f32, lane: char,direction:Direction) -> Self {
+        let velocity = distance / time;
+        Vehicle {
+            id,
+            distance,
+            total_distance: distance,
+            time,
+            velocity,
+            lane,
+            direction,
+        }
+    }
+
+    pub fn update(&mut self, delta_time: f32) {
+        if self.time > 0.0 {
+            self.distance -= self.velocity * delta_time;
+            self.time -= delta_time;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_physics_velocity() {
+        let car =  Vehicle::new(1, 100.0, 10.0, 's', Direction::North);
+        assert_eq!(car.velocity,10.0);
+    }
+    #[test]
+    fn test_physics_update(){
+        let mut car = Vehicle::new(1, 100.0, 10.0, 's', Direction::North);
+        car.update(1.0);
+        assert_eq!(car.distance,90.0);
+        assert_eq!(car.time,9.0);
+    }
+}
